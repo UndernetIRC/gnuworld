4ea740c1de389bcfca9fcb155f330cd9d0f33894
asoriano <asoriano>
2000-07-12 20:56:40 +0000

    Removed module code; added config file for client info and SQL database info; added some commenting.

--- src/cservice.cc
+++ src/cservice.cc
@@ -7,14 +7,14 @@
 #include	<cstdlib>
 
 #include	"client.h" 
-#include    "cservice.h"
+#include  	"cservice.h"
 #include	"EConfig.h"
 #include	"ip.h"
 #include	"Network.h"
 #include	"StringTokenizer.h"
 #include	"misc.h"
 #include	"ELog.h" 
-#include    "libpq++.h"
+#include        "libpq++.h"
 
 using std::vector ;
 using std::endl ;
@@ -25,93 +25,100 @@ using std::string ;
 namespace gnuworld
 {
 
-extern "C"
-{
-  xClient* _gnuwinit(const string& args)
-  { 
-    return new cservice(args);
-  }
-
-} 
+//-- Connect to the PG SQL database and load the configuration file.
  
 cservice::cservice(const string& args)
 { 
-  /*
-   *  Open a connection (via TCP) to the postgres server.
-   */
+	//-- Load in our cservice configuration file.
+	cserviceConfig = new EConfig( args ) ;
+	
+	string Query = "host=" + cserviceConfig->Require( "sql_host" )->second +
+		      " dbname=" + cserviceConfig->Require( "sql_db" )->second ;
 
-  SQLDb = new PgDatabase("host=defiant.atomicrevs.net dbname=cservice");
+	SQLDb = new PgDatabase( Query.c_str() ) ;
 
-  if (SQLDb->ConnectionBad()) {
-    elog << "[SQL]: Unable to connect to SQL server." << endl
-         << "[SQL]: Postgres error message: " << SQLDb->ErrorMessage() << endl;
-    exit(0);
-  } else { 
-    elog << "[SQL]: Connection established to SQL server." << endl;
-  }
+	//-- Make sure we connected to the SQL database; if we didn't we exit entirely.
 
-  nickName = "W";
-  userName = "cservice";
-  hostName = "undernet.org";
-  userDescription = "For help type: /msg W help"; 
-  Mode("+idk");
+	if (SQLDb->ConnectionBad ())
+	{
+		elog << "[SQL]: Unable to connect to SQL server." << endl 
+		     << "[SQL]: PostgreSQL error message: " << SQLDb->ErrorMessage () << endl ;
+
+		::exit( 0 ) ;
+	}
+	else
+	{
+		elog << "[SQL]: Connection established to SQL server." << endl ;
+	}
+
+	//-- Retrieve user, nick, host, and description from the configuration file -- these are all REQUIRED
+	//   and the program will exit unless these are present in cservice.conf!
+
+	nickName = cserviceConfig->Require( "nick" )->second ;
+	userName = cserviceConfig->Require( "user" )->second ;
+	hostName = cserviceConfig->Require( "host" )->second ;
+	userDescription = cserviceConfig->Require( "description" )->second ;
+
+	//-- Move this to the configuration file?  This should be fairly static..
+
+	Mode( "+idk" ) ;
 }
 
 cservice::~cservice()
 {
+	delete cserviceConfig ;	
 }
 
 int cservice::BurstChannels()
 {
-ExecStatusType status; // PGSql Status report container.
+	ExecStatusType status; 
  
-  /* 
-   *  Execute statement to retrieve all channels.
-   */
-  if ((status = SQLDb->Exec( STMT_ALLCHANS )) == PGRES_TUPLES_OK)
-  {
-    for (int i=0; i < SQLDb->Tuples(); i++) { 
-      StringTokenizer data( SQLDb->GetValue(i,0) ) ; 
-      MyUplink->JoinChannel(this, data[0], "+tn");
-    } 
-  }
- 
-  return xClient::BurstChannels(); 
-}
+	//-- We need to join every channel in the database that has been ADDCHAN'd by the
+	//   manager.  Various other things must be done, such as setting the topic if AutoTopic
+	//   is on, gaining ops if AlwaysOp is on, and so forth.
 
+	if ((status = SQLDb->Exec( "select channel from channels" )) == PGRES_TUPLES_OK)
+	{
+		for (int i = 0 ; i < SQLDb->Tuples (); i++)
+		{
+			StringTokenizer data( SQLDb->GetValue( i, 0 ) ) ;
+			MyUplink->JoinChannel( this, data[ 0 ], "+tn" ) ;
+		}
+	}
+
+	return xClient::BurstChannels () ;
+}
+	
 int cservice::OnConnect()
 {
-  return 0;
+	return 0;
 }
 
 int cservice::OnPrivateMessage( iClient* theClient, const string& Message )
 { 
-StringTokenizer st(Message);
-ExecStatusType status; // PGSql Status report container.
- 
-if( st.empty() )
+	StringTokenizer	st( Message ) ;
+	ExecStatusType	status ;
+
+	if( st.empty () )
 	{
-	return 0 ;
+		return 0 ;
 	}
 
-string command(string_upper(st[0]));
+	string	command( string_upper( st[ 0 ] ) ) ;
 
-if( command == "DUMPDB" )
-{
-  Notice( theClient, "All Channels in the Database:") ;
-  if ( (status = SQLDb->Exec( STMT_ALLCHANS )) == PGRES_TUPLES_OK ) 
-  {
-    for (int i=0; i < SQLDb->Tuples(); i++) {
-      strstream s;
-      StringTokenizer data(SQLDb->GetValue(i,0)) ;
-      s << "Name: " << data[0] << ends;
-      Notice(theClient, s.str()); 
-      delete[] s.str();
-    }
-  }
-}
+	// if( command == "COMMAND" ) 
+	// {
+	//	...perform command actions...
+	// }
 
-return 0 ;
+	if( command == "DUMP" )
+	{
+		string	dumpMe	= st.assemble( 1 ) ;
+		QuoteAsServer( dumpMe ) ;
+	}
+
+
+	return 0 ;
 }
 
 } // namespace gnuworld
