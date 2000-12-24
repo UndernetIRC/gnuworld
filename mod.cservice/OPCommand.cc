4b920eac565799a248724b587551d193d45ea3a6
Gte <gte@undernet.org>
2000-12-24 17:05:07 +0000

    Updated OP/VOICE to use new xClient method for opping multiple people.

--- mod.cservice/OPCommand.cc
+++ mod.cservice/OPCommand.cc
@@ -8,7 +8,7 @@
 #include	"Network.h"
 #include	"levels.h"
 
-const char OPCommand_cc_rcsId[] = "$Id: OPCommand.cc,v 1.5 2000/12/24 02:25:49 gte Exp $" ;
+const char OPCommand_cc_rcsId[] = "$Id: OPCommand.cc,v 1.6 2000/12/24 17:05:07 gte Exp $" ;
 
 namespace gnuworld
 {
@@ -18,7 +18,9 @@ using namespace level;
  
 bool OPCommand::Exec( iClient* theClient, const string& Message )
 { 
+	vector< iClient* > opList; // List of clients to op. 
 	StringTokenizer st( Message ) ;
+ 
 	if( st.size() < 2 )
 	{
 		Usage(theClient);
@@ -26,59 +28,81 @@ bool OPCommand::Exec( iClient* theClient, const string& Message )
 	}
  
 	/* 
-	 *  Check the channel exists.
+	 *  Check the channel is actually registered.
 	 */
 
 	sqlChannel* theChan = bot->getChannelRecord(st[1]);
-
-	if (theChan) {
-
-		/*
-		 *  Fetch the sqlUser record attached to this client. If there isn't one,
-		 *  they aren't logged in.
-		 */
-
-		sqlUser* theUser = bot->isAuthed(theClient, true);
-		if (!theUser) {
-			return false;
-		}
- 
-		// Check this user has access.
-		int level = bot->getAccessLevel(theUser, theChan);
-		if (level > level::op) 
-		{
-			iClient* target = Network->findNick(st[2]);
-			if( target == NULL )
-			{
-				bot->Notice( theClient, "I don't see %s anywhere.", st[2].c_str());
-				return true;
-			}
-
-			Channel* tmpChan = Network->findChannel(theChan->getName());
-			if (tmpChan) 
-			{
-				ChannelUser* tmpChanUser = tmpChan->findUser(target) ;
-				if (tmpChanUser) {
-					bot->Op(tmpChan, target);
-					elog << "opping" << endl;
-				} else {
-	 				bot->Notice(theClient, "Sorry, %s isn't on %s.", target->getNickName().c_str(), theChan->getName().c_str()); 
-					return false;
-				} 
-			} else {
-				bot->Notice(theClient, "Sorry, that channel is empty.");
-				return false;
-			} 
-		} else {
-			bot->Notice(theClient, "Sorry, you have insufficient access to perform that command.");
-			return false;
-		} 
-
-	} else {
+	if (!theChan) {
 		bot->Notice(theClient, "Sorry, %s isn't registered with me.", st[1].c_str());
 		return false;
+	} 
+
+	/*
+	 *  Fetch the sqlUser record attached to this client. If there isn't one,
+	 *  they aren't logged in - tell them they should be.
+	 */
+
+	sqlUser* theUser = bot->isAuthed(theClient, true);
+	if (!theUser) {
+		return false;
+	}
+ 
+	/*
+	 *  Check the user has sufficient access on this channel.
+	 */
+
+	int level = bot->getAccessLevel(theUser, theChan);
+	if (level < level::op)
+	{
+		bot->Notice(theClient, "Sorry, you have insufficient access to perform that command.");
+		return false;
+	} 
+
+	Channel* tmpChan = Network->findChannel(theChan->getName()); 
+	if (!tmpChan) 
+	{
+		bot->Notice(theClient, "Sorry, %s is empty.", theChan->getName().c_str());
+		return false;
+	}
+ 
+
+	if( st.size() < 3 ) // No nicks provided, assume we op ourself. :)
+	{
+		opList.push_back(theClient);
 	}
 
+	/*
+	 *  Loop over the remaining 'nick' parameters, opping them all.
+	 */
+
+	iClient* target;
+	unsigned short counter = 2; // Offset of first nick in list.
+	unsigned short cont = true;
+
+	while (counter < st.size())
+	{ 
+		target = Network->findNick(st[counter]);
+
+		if(!target)
+		{
+			bot->Notice(theClient, "I don't see %s anywhere.", st[counter].c_str());
+			cont = false;
+		} else { 
+			ChannelUser* tmpChanUser = tmpChan->findUser(target) ; 
+			if (!tmpChanUser) 
+			{
+				bot->Notice(theClient, "Sorry, %s isn't on %s.", target->getNickName().c_str(), theChan->getName().c_str()); 
+				cont = false;
+			}
+		}
+		
+	 	if (cont) opList.push_back(target);
+		cont = true;
+		counter++;
+	}
+
+	// Op them. 
+	bot->Op(tmpChan, opList);
 	return true ;
 } 
 
--- mod.cservice/VOICECommand.cc
+++ mod.cservice/VOICECommand.cc
@@ -8,7 +8,7 @@
 #include	"Network.h"
 #include	"levels.h"
 
-const char VOICECommand_cc_rcsId[] = "$Id: VOICECommand.cc,v 1.4 2000/12/24 02:25:49 gte Exp $" ;
+const char VOICECommand_cc_rcsId[] = "$Id: VOICECommand.cc,v 1.5 2000/12/24 17:05:07 gte Exp $" ;
 
 namespace gnuworld
 {
@@ -18,7 +18,9 @@ using namespace level;
  
 bool VOICECommand::Exec( iClient* theClient, const string& Message )
 { 
+	vector< iClient* > voiceList; // List of clients to voice. 
 	StringTokenizer st( Message ) ;
+ 
 	if( st.size() < 2 )
 	{
 		Usage(theClient);
@@ -26,48 +28,81 @@ bool VOICECommand::Exec( iClient* theClient, const string& Message )
 	}
  
 	/* 
-	 *  Check the channel exists.
+	 *  Check the channel is actually registered.
 	 */
 
 	sqlChannel* theChan = bot->getChannelRecord(st[1]);
-
-	if (theChan) {
-
-		/*
-		 *  Fetch the sqlUser record attached to this client. If there isn't one,
-		 *  they aren't logged in.
-		 */
-
-		sqlUser* theUser = bot->isAuthed(theClient, true);
-		if (!theUser) {
-			return false;
-		}
- 
-		// Check this user has access.
-		int level = bot->getAccessLevel(theUser, theChan);
-		if (level > level::voice) 
-		{
-			iClient* target = Network->findNick(st[2]);
-			if( target == NULL )
-			{
-				bot->Notice( theClient, "I don't see %s anywhere.", st[2].c_str());
-				return true;
-			}
-
-			Channel* tmpChan = Network->findChannel(theChan->getName());
-			if (tmpChan) {
-// TODO:				bot->Voice(tmpChan, target);
-			} else {
-				bot->Notice(theClient, "Sorry, that channel is empty.");
-			} 
-		} else {
-			bot->Notice(theClient, "Sorry, you have insufficient access to perform that command.");
-		}
-
-	} else {
+	if (!theChan) {
 		bot->Notice(theClient, "Sorry, %s isn't registered with me.", st[1].c_str());
+		return false;
+	} 
+
+	/*
+	 *  Fetch the sqlUser record attached to this client. If there isn't one,
+	 *  they aren't logged in - tell them they should be.
+	 */
+
+	sqlUser* theUser = bot->isAuthed(theClient, true);
+	if (!theUser) {
+		return false;
+	}
+ 
+	/*
+	 *  Check the user has sufficient access on this channel.
+	 */
+
+	int level = bot->getAccessLevel(theUser, theChan);
+	if (level < level::voice)
+	{
+		bot->Notice(theClient, "Sorry, you have insufficient access to perform that command.");
+		return false;
+	} 
+
+	Channel* tmpChan = Network->findChannel(theChan->getName()); 
+	if (!tmpChan) 
+	{
+		bot->Notice(theClient, "Sorry, %s is empty.", theChan->getName().c_str());
+		return false;
+	}
+ 
+
+	if( st.size() < 3 ) // No nicks provided, assume we voice ourself. :)
+	{
+		voiceList.push_back(theClient);
 	}
 
+	/*
+	 *  Loop over the remaining 'nick' parameters, voiceing them all.
+	 */
+
+	iClient* target;
+	unsigned short counter = 2; // Offset of first nick in list.
+	unsigned short cont = true;
+
+	while (counter < st.size())
+	{ 
+		target = Network->findNick(st[counter]);
+
+		if(!target)
+		{
+			bot->Notice(theClient, "I don't see %s anywhere.", st[counter].c_str());
+			cont = false;
+		} else { 
+			ChannelUser* tmpChanUser = tmpChan->findUser(target) ; 
+			if (!tmpChanUser) 
+			{
+				bot->Notice(theClient, "Sorry, %s isn't on %s.", target->getNickName().c_str(), theChan->getName().c_str()); 
+				cont = false;
+			}
+		}
+		
+	 	if (cont) voiceList.push_back(target);
+		cont = true;
+		counter++;
+	}
+
+	// Voice them. 
+//	bot->Voice(tmpChan, voiceList);
 	return true ;
 } 
 
