9bc2d584e05526ce612172163542b5e22e11461e
Dan Karrels <dan@undernet.org>
2001-02-23 19:02:01 +0000

    Minor mod

--- mod.cservice/constants.h
+++ mod.cservice/constants.h
@@ -1,5 +1,5 @@
 #ifndef __CONSTANTS_H
-#define __CONSTANTS_H "$Id: constants.h,v 1.10 2001/02/18 19:46:01 dan_karrels Exp $"
+#define __CONSTANTS_H "$Id: constants.h,v 1.11 2001/02/23 19:02:01 dan_karrels Exp $"
 
 /* 
  * constants.h
@@ -9,22 +9,22 @@
  * 
  * Defines constants used throughout the application.
  *
- * $Id: constants.h,v 1.10 2001/02/18 19:46:01 dan_karrels Exp $
+ * $Id: constants.h,v 1.11 2001/02/23 19:02:01 dan_karrels Exp $
  */
 
 namespace gnuworld 
 { 
-	namespace sql
-		{
-		/*
-		 *  Comma seperated lists of fields for use in retrieving various
-		 *  articles of data.
-		 */
-		const string channel_fields = "id,name,flags,mass_deop_pro,flood_pro,url,description,keywords,registered_ts,channel_ts,channel_mode,userflags,last_updated";
-		const string user_fields = "id,user_name,password,email,url,language_id,public_key,flags,last_updated_by,users.last_updated,users_lastseen.last_seen";
-		const string level_fields = "channel_id,user_id,access,flags,suspend_expires,suspend_by,added,added_by,last_Modif,last_Modif_By,last_Updated";
-		const string ban_fields = "id,channel_id,banmask,set_by,set_ts,level,expires,reason,last_updated";
-		}
+namespace sql
+	{
+	/*
+	 *  Comma seperated lists of fields for use in retrieving various
+	 *  articles of data.
+	 */
+	const string channel_fields = "id,name,flags,mass_deop_pro,flood_pro,url,description,keywords,registered_ts,channel_ts,channel_mode,userflags,last_updated";
+	const string user_fields = "id,user_name,password,email,url,language_id,public_key,flags,last_updated_by,users.last_updated,users_lastseen.last_seen";
+	const string level_fields = "channel_id,user_id,access,flags,suspend_expires,suspend_by,added,added_by,last_Modif,last_Modif_By,last_Updated";
+	const string ban_fields = "id,channel_id,banmask,set_by,set_ts,level,expires,reason,last_updated";
+	}
 }
 
 #endif
