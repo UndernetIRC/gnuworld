e6e43484e694337a5e2f9552bdda451415a5f82c
Gte <gte@undernet.org>
2001-03-29 18:14:12 +0000

    comment out addition of users_lastseen record, because the new function automatically adds them :)

--- doc/cservice.addme.sql
+++ doc/cservice.addme.sql
@@ -1,4 +1,4 @@
--- "$Id: cservice.addme.sql,v 1.3 2001/02/23 20:19:43 mrbean_ Exp $"
+-- "$Id: cservice.addme.sql,v 1.4 2001/03/29 18:14:12 gte Exp $"
 -- 2001-02-23 : |MrBean|
 -- This script will reigster the admin channel * 
 -- And will create an admin by the name of Admin  
@@ -11,11 +11,10 @@ VALUES ('*',1,now()::abstime::int4,31337,now()::abstime::int4);
 INSERT into users (user_name,password,language_id,last_updated) 
 VALUES ('Admin','07d976a29bdb3ff42e19c102852151b28097bbad',1,now()::abstime::int4);
 
-INSERT into users_lastseen (user_id,last_seen,last_updated) 
-VALUES (1,now()::abstime::int4,now()::abstime::int4);
+--INSERT into users_lastseen (user_id,last_seen,last_updated) 
+--VALUES (1,now()::abstime::int4,now()::abstime::int4);
 
 INSERT into levels (channel_id,user_id,access,last_updated)
 VALUES (1,1,1000,now()::abstime::int4);
 
 -- END HERE
-
