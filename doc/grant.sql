6522c57b4308542c441bb68b6fbb524aad5a4b5a
Gte <gte@undernet.org>
2001-04-01 00:01:36 +0000

    Add grant file

---
+++ doc/grant.sql
@@ -0,0 +1,22 @@
+-- Grant access to the username which will access the database.
+-- Use this to grant full access to all tables for the username
+-- 'gnuworld'.
+
+grant all on languages to gnuworld;
+grant all on translations to gnuworld; 
+grant all on help to gnuworld;
+grant all on channels to gnuworld;
+grant all on bans to gnuworld;
+grant all on users to gnuworld;
+grant all on users_lastseen to gnuworld;
+grant all on levels to gnuworld;
+grant all on channellog to gnuworld;
+grant all on log_users to gnuworld;
+grant all on userlog to gnuworld;
+grant all on supporters to gnuworld;
+grant all on pending to gnuworld;
+grant all on domain to gnuworld;
+grant all on webcookies to gnuworld;
+grant all on lastrequests to gnuworld;
+grant all on pendingusers to gnuworld;
+grant all on transition to gnuworld;
