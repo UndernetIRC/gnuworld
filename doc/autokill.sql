5a4dd3333ad4a36b7ef01955a9108dc222b500bd
Gte <gte@undernet.org>
2001-09-01 00:23:17 +0000

    Update autokill script a little.

--- doc/autokill.sql
+++ doc/autokill.sql
@@ -1,7 +1,7 @@
 -- Select all user_id's idle > 90 days into a temp table.
 --
 \qecho [*] Fetching list of idle user accounts:
-SELECT user_id,last_seen INTO TEMP TABLE to_die FROM users_lastseen WHERE (last_seen <= now()::abstime::int4 - (86400 * 90));
+SELECT user_id,last_seen INTO TABLE to_die FROM users_lastseen WHERE (last_seen <= now()::abstime::int4 - (86400 * 90));
 --
 -- Remove any who are channel managers, they can be delt with seperately and are not auto-purged.
 \qecho [*] Discounting those who are channel managers:
@@ -25,6 +25,10 @@ DELETE FROM levels where user_id = to_die.user_id;
 \qecho [*] Removing supporter records..
 DELETE FROM supporters where user_id = to_die.user_id;
 --
+-- Clean up objection records.
+\qecho [*] Removing Objection records..
+DELETE FROM objections where user_id = to_die.user_id;
+--
 -- Clean up last_seens.
 \qecho [*] Removing last_seens..
 DELETE FROM users_lastseen where user_id = to_die.user_id;
@@ -32,3 +36,6 @@ DELETE FROM users_lastseen where user_id = to_die.user_id;
 -- Clean up users (Finally!).
 \qecho [*] Removing user accounts..
 DELETE FROM users where id = to_die.user_id;
+--
+-- Clean up the temporary table.
+DROP TABLE to_die;
