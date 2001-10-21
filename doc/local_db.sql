64ccaa8c965129142a585dbc7babdbae97196d57
nighty <nighty@undernet.org>
2001-10-21 02:17:14 +0000

    *** empty log message ***

--- doc/local_db.sql
+++ doc/local_db.sql
@@ -1,5 +1,5 @@
 ------------------------------------------------------------------------------------
--- "$Id: local_db.sql,v 1.1 2001/10/16 00:31:46 nighty Exp $"
+-- "$Id: local_db.sql,v 1.2 2001/10/21 02:17:14 nighty Exp $"
 -- Channel service DB SQL file for PostgreSQL.
 --
 -- Tables specific to webcookies (needs to be LOCAL).
@@ -32,4 +32,13 @@ CREATE INDEX pending_pwreset_cookie_idx ON pending_pwreset(cookie);
 CREATE INDEX pending_pwreset_user_id_idx ON pending_pwreset(user_id);
 CREATE INDEX pending_pwreset_expiration_idx ON pending_pwreset(expiration);
 
+CREATE TABLE helpmgr_users (
+	user_id INT4 NOT NULL,
+	language_id INT2,
+	flags INT2 DEFAULT '0'
+);
+
+CREATE INDEX helpmgr_users_user_id_idx ON helpmgr_users(user_id);
+CREATE INDEX helpmgr_language_id_idx ON helpmgr_users(language_id);
+CREATE INDEX helpmgr_flags_idx ON helpmgr_users(flags);
 
