6cf10b2e3d1fc02d107a24ada150fe3070990a7a
nighty <nighty@undernet.org>
2001-12-24 02:18:10 +0000

    *** empty log message ***

--- doc/languages.sql
+++ doc/languages.sql
@@ -3,19 +3,19 @@
 
 DELETE FROM languages; 
 COPY "languages" FROM stdin;
-1	EN	English	31337
-2	FR	French	31337
-3	DK	Danish	31337
-4	NL	Dutch	31337
-5	DE	German	31337
-6	PT	Portuguese	31337
-7	IT	Italian	31337
-8	RO	Romanian	31337
-9	CA	Catalan	31337
-10	ES	Spanish	31337
-11	HU	Hungary	31337
-12	TR	Turkish	31337
-13	NO	Norsk	31337
-14	AR	Arabic	31337
-15	HA	Haloween	31337
+1	EN	English	31337	0
+2	FR	French	31337	0
+3	DK	Danish	31337	0
+4	NL	Dutch	31337	0
+5	DE	German	31337	0
+6	PT	Portuguese	31337	0
+7	IT	Italian	31337	0
+8	RO	Romanian	31337	0
+9	CA	Catalan	31337	0
+10	ES	Spanish	31337	0
+11	HU	Hungary	31337	0
+12	TR	Turkish	31337	0
+13	NO	Norsk	31337	0
+14	AR	Arabic	31337	0
+15	HA	Haloween	31337	0
 \.
