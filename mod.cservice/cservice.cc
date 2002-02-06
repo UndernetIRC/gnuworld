2b4f78375212d8fa23f4faac37b4f2a9a1fa6300
Gte <gte@undernet.org>
2002-02-06 19:12:08 +0000

    Bump PL.

--- mod.cservice/cservice.cc
+++ mod.cservice/cservice.cc
@@ -751,7 +751,7 @@ else if(Command == "VERSION")
 	xClient::DoCTCP(theClient, CTCP,
 		"Undernet P10 Channel Services II ["
 		__DATE__ " " __TIME__
-		"] Release 1.1pl5");
+		"] Release 1.1pl6");
 	}
 else if(Command == "PROBLEM?")
 	{
