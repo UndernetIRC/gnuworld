014bb8454332c5e594317d9cf15d8e08e46cd2c2
Dan Karrels <dan@undernet.org>
2002-05-27 17:53:04 +0000

    Added license to top of each file

--- lib/Connection.cc
+++ lib/Connection.cc
@@ -1,7 +1,24 @@
 /**
  * Connection.cc
  * Author: Daniel Karrels (dan@karrels.com)
- * $Id: Connection.cc,v 1.1 2002/05/27 17:22:02 dan_karrels Exp $
+ * Copyright (C) 2002 Daniel Karrels <dan@karrels.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
+ * USA.
+ *
+ * $Id: Connection.cc,v 1.2 2002/05/27 17:53:04 dan_karrels Exp $
  */
 
 #include	<sys/types.h>
