a1e7578f60b6a54ae6a520c20d09ae909a9437da
Dan Karrels <dan@undernet.org>
2002-05-28 20:27:26 +0000

    readded the copyright stuff

--- lib/ConnectionManager.h
+++ lib/ConnectionManager.h
@@ -1,11 +1,28 @@
 /**
  * ConnectionManager.h
  * Author: Daniel Karrels (dan@karrels.com)
- * $Id: ConnectionManager.h,v 1.3 2002/05/28 20:25:48 dan_karrels Exp $
+ * Copyright (C) 2002 Daniel Karrels <dan@karrels.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
+ * USA.
+ *
+ * $Id: ConnectionManager.h,v 1.4 2002/05/28 20:27:26 dan_karrels Exp $
  */
 
 #ifndef __CONNECTIONMANAGER_H
-#define __CONNECTIONMANAGER_H "$Id: ConnectionManager.h,v 1.3 2002/05/28 20:25:48 dan_karrels Exp $"
+#define __CONNECTIONMANAGER_H "$Id: ConnectionManager.h,v 1.4 2002/05/28 20:27:26 dan_karrels Exp $"
 
 #include	<sys/types.h>
 
