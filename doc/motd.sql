1844f58bdf442031b233044a9bacedf4e9604b42
Reed Loden <reed@redmagnet.com>
2002-08-09 01:19:58 +0000

    More work on doc/.

--- doc/motd.sql
+++
@@ -1,8 +0,0 @@
--- $Id: motd.sql,v 1.3 2002/01/13 07:44:44 nighty Exp $
--- Used to store MOTD text. Use \n for multiple lines.
--- MOTD is just a language response with id 9999.
-
-DELETE FROM translations where response_id = 9999;
-COPY "translations" FROM stdin; 
-1	9999	\026Undernet Channel Services MOTD\026\n\nService Announcements:\nNone at present.	31337
-\.
--- doc/update.delete.sql
+++
@@ -1,3 +0,0 @@
--- "Unregister" previously "deleted" channels (Purged).
-
-UPDATE channels SET registered_ts = 0 WHERE deleted = 1;
