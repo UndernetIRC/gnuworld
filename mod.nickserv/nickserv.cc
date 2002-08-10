ce76b3419c528dd1cbc213bbfadad55082257972
Jeekay <jeekay@undernet.org>
2002-08-10 19:54:51 +0000

    Functionality! Works perfectly as far as I can see, except you cant log in. Ah well

--- mod.nickserv/nickserv.cc
+++ mod.nickserv/nickserv.cc
@@ -7,9 +7,10 @@
 #include "client.h"
 #include "server.h"
 
+#include "netData.h"
 #include "nickserv.h"
 
-const char NickServ_cc_rcsId[] = "$Id: nickserv.cc,v 1.1 2002/08/10 13:55:19 jeekay Exp $";
+const char NickServ_cc_rcsId[] = "$Id: nickserv.cc,v 1.2 2002/08/10 19:54:51 jeekay Exp $";
 
 namespace gnuworld
 {
@@ -46,6 +47,10 @@ assert(nickservConfig != 0);
 /* Load in the values we need from the config file */
 consoleChannel = nickservConfig->Require("consoleChannel")->second;
 
+/* Load in the processing queue variables */
+checkFreq = atoi(nickservConfig->Require("checkFreq")->second.c_str());
+startDelay = atoi(nickservConfig->Require("startDelay")->second.c_str());
+
 string dbHost = nickservConfig->Require("dbHost")->second;
 string dbPort = nickservConfig->Require("dbPort")->second;
 string dbDb = nickservConfig->Require("dbDb")->second;
@@ -56,6 +61,9 @@ string dbString = "host=" + dbHost + " port=" + dbPort + " dbname=" + dbDb
   + " user="+dbUser + " password=" + dbPass;
 
 theManager = sqlManager::getInstance(dbString);
+
+/* Precache the users */
+precacheUsers();
 }
 
 nickserv::~nickserv()
@@ -63,24 +71,13 @@ nickserv::~nickserv()
 delete nickservConfig;
 }
 
-void nickserv::ImplementServer( xServer* theServer )
-{
-theServer->RegisterEvent(EVT_KILL, this);
-theServer->RegisterEvent(EVT_QUIT, this);
-theServer->RegisterEvent(EVT_NICK, this);
-theServer->RegisterEvent(EVT_ACCOUNT, this);
 
-xClient::ImplementServer( theServer );
-}
 
-int nickserv::OnPrivateMessage( iClient* theClient,
-	const string& message, bool )
-{
-Notice( theClient, "Howdy :)" ) ;
-return 0 ;
-}
-
-// Burst any channels.
+/**
+ * Here we decide what channels our xClient needs to burst in to.
+ * The only channel of any interest to us is our console channel, which is
+ * loaded from the configuration file.
+ */
 int nickserv::BurstChannels()
 {
 MyUplink->JoinChannel(this, consoleChannel, nickservConfig->Require("consoleChannelModes")->second);
@@ -88,18 +85,252 @@ MyUplink->JoinChannel(this, consoleChannel, nickservConfig->Require("consoleChan
 return xClient::BurstChannels() ;
 }
 
+
+/**
+ * Here we set up the various bits and pieces that we need an xServer
+ * reference to be able to do.
+ */
+void nickserv::ImplementServer( xServer* theServer )
+{
+/* Register for all the events we want to see */
+theServer->RegisterEvent(EVT_ACCOUNT, this);
+theServer->RegisterEvent(EVT_CHNICK, this);
+theServer->RegisterEvent(EVT_KILL, this);
+theServer->RegisterEvent(EVT_NICK, this);
+theServer->RegisterEvent(EVT_QUIT, this);
+
+/* Register timerID's to process queues */
+
+/* The timer to kick off the processing of the queue
+ * Note: We add startDelay here to allow for a longer delay before checking
+ * when we first link to the net
+ */
+time_t theTime = time(NULL) + startDelay;
+processQueue_timerID = theServer->RegisterTimer(theTime, this, NULL);
+
+xClient::ImplementServer( theServer );
+}
+
+
+/**
+ * Here we deal with any network events that we have asked to listen for.
+ * The main jobs done are:
+ *  EVT_NICK  : Instantiate a netData object and assign it to the iClient.
+ *              Add the iClient to the process queue.
+ *  EVT_CHNICK: If the iClient isnt in the process queue, readd it.
+ *              We do NOT zero the warning count. This is to prevent someone
+ *                jumping between registered nicks to avoid getting killed.
+ *  EVT_KILL  : Delete the netData instance
+ *              Remove the iClient from the process queue
+ *  EVT_QUIT  : Delete the netData instance
+ *              Remove the iClient from the process queue
+ */
+int nickserv::OnEvent( const eventType& event, void* Data1, void* Data2, void* Data3, void* Data4)
+{
+
+/* The target user of the event */
+iClient* theClient = static_cast< iClient* >( Data1 );
+
+switch( event ) {
+  case EVT_KILL: theClient = static_cast< iClient* >( Data2 );
+  case EVT_QUIT: {
+    netData* theData = static_cast< netData* >( theClient->removeCustomData(this) );
+    delete theData;
+    removeFromQueue(theClient);
+    
+    return 1;
+    break;
+  } // case EVT_KILL/QUIT
+  
+  case EVT_NICK: {
+    netData* theData = new netData();
+    theClient->setCustomData(this, theData);
+    addToQueue(theClient);
+    
+    return 1;
+    break;
+  } // case EVT_NICK
+  
+  case EVT_CHNICK: {
+    addToQueue(theClient);
+    
+    return 1;
+    break;
+  }
+} // switch( event )
+
+return 0;
+}
+
+
+/**
+ * Here we deal with incoming communications from network clients.
+ */
+int nickserv::OnPrivateMessage( iClient* theClient,
+	const string& message, bool )
+{
+Notice( theClient, "Howdy :)" ) ;
+return 0 ;
+}
+
+
+
+/**
+ * When a timer expires, this function is called. It allows for periodic
+ * processing of data.
+ */
+int nickserv::OnTimer(xServer::timerID theTimer, void* theData)
+{
+if(theTimer == processQueue_timerID) {
+  processQueue();
+  
+  time_t theTime = time(NULL) + checkFreq;
+  processQueue_timerID = MyUplink->RegisterTimer(theTime, this, NULL);
+  return 1;
+} // if(theTimer == processQueue_timerID)
+
+return 0;
+} // nickserv::OnTimer(xServer::timerID, void*)
+
+
+
 /**
  * This function empties the current user cache and reloads all information from
  * the database. It is only called at startup, as the cache is authoritative.
  */
 void nickserv::precacheUsers()
 {
-  /* Get a connection instance to our backend */
-  PgDatabase* cacheCon = theManager->getConnection();
-  
-  /* Dispose of our connection instance */
-  theManager->removeConnection(cacheCon);
+elog << "*** [NickServ:precacheUsers] Precaching users." << endl;
+
+/* Get a connection instance to our backend */
+PgDatabase* cacheCon = theManager->getConnection();
+
+/* Retrieve the list of registered users */
+string cacheQuery = "SELECT id,name,flags,level FROM users";
+if(cacheCon->ExecTuplesOk(cacheQuery.c_str())) {
+  for(int i = 0; i < cacheCon->Tuples(); i++) {
+    sqlUser* tmpUser = new sqlUser();
+    tmpUser->setAllMembers(cacheCon, i);
+    sqlUserCache.insert(sqlUserHashType::value_type(tmpUser->getName(), tmpUser));
+  }
+} else {
+  elog << "*** [NickServ:precacheUsers] Error executing query: "
+    << cacheCon->ErrorMessage()
+    << endl;
+  ::exit(0);
 }
+
+elog << "*** [NickServ:precacheUsers] Done. Loaded a total of "
+  << sqlUserCache.size() << " user records."
+  << endl;
+
+/* Dispose of our connection instance */
+theManager->removeConnection(cacheCon);
+}
+
+
+/**
+ * This function attempts to add a given iClient to the processing queue.
+ * It will return 1 if the client did not already exist in the queue, else 0.
+ * It is safe to ignore the return value.
+ */
+int nickserv::addToQueue(iClient* theClient)
+{
+QueueType::iterator queuePos =
+  find(warnQueue.begin(), warnQueue.end(), theClient);
+if(queuePos != warnQueue.end()) {
+  return 0;
+} else {
+  warnQueue.push_back(theClient);
+  return 1;
+}
+} // nickserv::addToQueue(iClient*)
+
+
+/**
+ * This function attempts to remove a given iClient from the processing queue.
+ * It will 1 if the client was actually found, else 0.
+ * It is safe to ignore the return value.
+ */
+int nickserv::removeFromQueue(iClient* theClient)
+{
+QueueType::iterator queuePos = 
+  find(warnQueue.begin(), warnQueue.end(), theClient);
+if(queuePos != warnQueue.end()) {
+  warnQueue.erase(queuePos);
+  return 1;
+} else {
+  return 0;
+}
+} // nickserv::removeFromQueue(iClient*)
+
+
+/**
+ * This function iterates through the processing queue and takes any
+ * action as appropriate.
+ */
+void nickserv::processQueue()
+{
+
+/* A number of things can happen here.
+ * Firstly, the warnQueue can be empty. If this is the case, return immediately.
+ */
+
+elog << "warnQueue size: " << warnQueue.size() << endl;
+
+if(warnQueue.size() == 0) { return; }
+
+QueueType killQueue;
+
+/* Now we start to process the queue. The rules are as follows for a given nick:
+ *  Is the cached user record set to autokill?
+ *    No  - remove entry from warnQueue, continue processing next element
+ *  Has the user already been warned?
+ *    Yes - add user to killQueue, remove from warnQueue
+ *    No  - warn user, increment warnings
+ *
+ * NOTE: The for() does NOT automatically increment queuePos so we must take care
+ *       to do it manually when required. This is to enable us to delete from the
+ *       warnQueue while we are iterating over it.
+ */
+
+for(QueueType::iterator queuePos = warnQueue.begin(); queuePos != warnQueue.end(); ) {
+  iClient* theClient = *queuePos;
+  netData* theData = static_cast< netData* >( theClient->getCustomData(this) );
+  
+  /* First things first - is this nick set to autokill? */
+  sqlUserHashType::const_iterator cachedUser = sqlUserCache.find(theClient->getNickName());
+  if(cachedUser == sqlUserCache.end() || !(cachedUser->second)->getFlag(sqlUser::F_AUTOKILL)) {
+    theData->warned = 0;
+    queuePos = warnQueue.erase(queuePos);
+    continue;
+  }
+  
+  if(theData->warned) {
+    killQueue.push_back(theClient);
+    queuePos = warnQueue.erase(queuePos);
+    continue;
+  } else {
+    Notice(theClient, "You are using a registered nickname. Please login or you will be disconnected.");
+    theData->warned++;
+    queuePos++;
+    continue;
+  } // if(theData->warned)
+} // iterate over warnQueue
+
+elog << "killQueue size: " << killQueue.size() << endl;
+
+if(killQueue.size() == 0) { return; }
+
+for(QueueType::iterator queuePos = killQueue.begin(); queuePos != killQueue.end(); queuePos++) {
+  iClient* theClient = *queuePos;
+  netData* theData = static_cast<netData*>(theClient->removeCustomData(this));
+  delete(theData);
+  Kill(theClient, "[NickServ] AutoKill");
+} // iterate over killQueue
+
+} // nickserv::processQueue()
+
 
 } // namespace ns
 
--- mod.nickserv/nickserv.h
+++ mod.nickserv/nickserv.h
@@ -1,9 +1,10 @@
 #ifndef _NICKSERV_H
-#define _NICKSERV_H "$Id: nickserv.h,v 1.1 2002/08/10 13:55:19 jeekay Exp $"
+#define _NICKSERV_H "$Id: nickserv.h,v 1.2 2002/08/10 19:54:51 jeekay Exp $"
 
 #include "EConfig.h"
 
 #include "sqlManager.h"
+#include "sqlUser.h"
 
 namespace gnuworld {
 
@@ -12,42 +13,105 @@ namespace ns {
 class nickserv : public xClient {
   public:
     
-    /**
-     * Constructor receives a configuration file name
-     */
+    /*******************************************************
+     ** O V E R R I D E N   X C L I E N T   M E T H O D S **
+     *******************************************************/
+    
+    /** Constructor receives a configuration file name */
     nickserv( const string& ) ;
     
-    /**
-     * Destructor to remove anything interesting we've done
-     */
+    /** Destructor to remove anything interesting we've done */
     virtual ~nickserv() ;
     
-    /**
-     * This method is called when the bot gets a PRIVMSG
-     */
-    virtual int OnPrivateMessage( iClient*, const string&, bool secure = false ) ;
-    
-    /**
-     * This method is called after server connection.
-     * Here we tell the xServer what channels we want to burst in to
-     */
+    /** This method is called after server connection */
     virtual int BurstChannels() ;
     
     /** This is called when we have attached to the xServer */
     virtual void ImplementServer( xServer* ) ;
 
+    /** This is called when a network event happens */
+    virtual int OnEvent( const eventType&, void* , void*, void*, void* );
+
+    /** This method is called when the bot gets a PRIVMSG */
+    virtual int OnPrivateMessage( iClient*, const string&, bool secure = false ) ;
+    
+    /** This method is called when a timer expires */
+    virtual int OnTimer(xServer::timerID, void*);
+    
+  
+    /*********************************
+     ** N I C K S E R V   T Y P E S **
+     *********************************/
+    
+    typedef map< string, sqlUser*, noCaseCompare > sqlUserHashType;
+    
+    typedef vector< iClient* > QueueType;
+    
+  
+    /*************************************
+     ** N I C K S E R V   M E T H O D S **
+     *************************************/
+
     /** Load all users into the user cache */
     void precacheUsers();
+    
+    /** Add an iClient to the processing queue */
+    int addToQueue(iClient*);
+    
+    /** Remove an iClient from the processing queue */
+    int removeFromQueue(iClient*);
+    
+    /** Process the queue */
+    void processQueue();
+
+    
+    /*****************************************
+     ** N I C K S E R V   V A R I A B L E S **
+     *****************************************/
 
     /** Our sqlManager instance for DB communication */
     sqlManager* theManager;
 
   protected:
+    /*************************************
+     ** C O N F I G   V A R I A B L E S **
+     *************************************/
+    
+    /** The frequency with which we process the queue */
+    int checkFreq;
+
+    /** Store the console channel name */
+    string consoleChannel;
+
     /** Store the config file pointer */
     EConfig* nickservConfig;
     
-    /** Store the console channel name */
-    string consoleChannel;
+    /** How long to wait after linking before processing */
+    int startDelay;
+    
+    
+    /*****************
+     ** Q U E U E S **
+     *****************/
+    
+    /** List of iClient's that currently need verifying */
+    QueueType warnQueue;
+    
+    /** The cached list of registered users */
+    sqlUserHashType sqlUserCache;
+    
+    
+    /***********************
+     ** T I M E R   I D S **
+     ***********************/
+     
+     /** TimerID for processing the queue */
+     xServer::timerID processQueue_timerID;
+     
+     
+     /*************************
+      ** S T A T I S T I C S **
+      *************************/
 }; // class nickserv
 
 } // namespace ns
