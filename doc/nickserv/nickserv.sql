d3224459bdba99ef6802a418fe85d58577e32933
Jeekay <jeekay@undernet.org>
2002-08-25 00:49:20 +0000

    New schema with default field values

--- doc/nickserv/nickserv.sql
+++ doc/nickserv/nickserv.sql
@@ -1,13 +1,14 @@
 -- Creation of tables needed for nickserv
 
 CREATE TABLE "users" (
-   "id" int4 DEFAULT nextval('"users_id_seq"'::text) NOT NULL,
-   "name" varchar(40) NOT NULL,
-   "lastseen_ts" int4 NOT NULL,
-   "registered_ts" int4 NOT NULL,
-   "flags" int4 NOT NULL,
-   "level" int2 NOT NULL,
-   CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+	"id" integer DEFAULT nextval('"users_id_seq"'::text) NOT NULL,
+	"name" character varying(40) NOT NULL,
+	"lastseen_ts" integer DEFAULT 0 NOT NULL,
+	"registered_ts" integer DEFAULT 0 NOT NULL,
+	"flags" integer DEFAULT 0 NOT NULL,
+	"level" smallint DEFAULT 0 NOT NULL,
+	Constraint "users_pkey" Primary Key ("id")
 );
-CREATE  INDEX "id_users_key" ON "users" ("id");
+
+CREATE INDEX id_users_key ON users USING btree (id);
 
