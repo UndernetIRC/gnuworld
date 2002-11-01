613d8c8b0302a0a905dd3dbf5ef73bd3afa9345f
nighty <nighty@undernet.org>
2002-11-01 15:09:21 +0000

    Changed default theme

--- doc/update_themes.sql
+++ doc/update_themes.sql
@@ -1,8 +1,13 @@
 --
--- $Id: update_themes.sql,v 1.3 2002/08/30 10:38:17 nighty Exp $
+-- $Id: update_themes.sql,v 1.4 2002/11/01 15:09:21 nighty Exp $
 -- Use this file to update your db structure if you dont have "themes" enabled.
 -- run /usr/local/pgsql/bin/psql local_db < update_themes.sql
 -- ** run this only ONCE **
+--
+-- If you want to update your theme, its advised to edit the 'website/docs/gnuworld/themes/data/default/default.sql' file
+-- and apply the changes using 'psql local_db < website/docs/gnuworld/themes/data/default/default.sql'
+--
+-- The default theme logo was done by SiRVulcaN <sirvulcan@xtra.co.nz>.
 --
 -- 05/21/2002 : <nighty@undernet.org>
 --
@@ -88,6 +93,7 @@ CREATE TABLE themes (
 
 );
 
+
 INSERT INTO themes VALUES (
 -- head
 '1',
@@ -100,35 +106,35 @@ INSERT INTO themes VALUES (
 'default',
 
 -- left
-'d0e5ff',
+'60659c',
 '',
 '000000',
 'aaaaaa',
-'0000ff',
+'ffffff',
 
 'aaaaaa',
 '000000',
-'0000ff',
+'ffff00',
 
 -- top
-'d0e5ff',
+'60659c',
 '',
-'top_logo.jpg',
+'default_logo.jpg',
 
 -- bottom (footer)
-'d0e5ff',
+'60659c',
 '',
-'4c4c4c',
-'000033',
-'0000aa',
+'000000',
+'aaaaaa',
+'ffffff',
 
 -- main
-'dddddd',
+'aaafe4',
 '',
 '000000',
 '505050',
-'004400',
-'0000ff',
+'60659c',
+'ff7700',
 'ff0000',
 
 '990000',
@@ -144,12 +150,12 @@ INSERT INTO themes VALUES (
 '990099',
 'ff0000',
 
-'ff1111',
+'60659c',
 
 '00ff00',
 'ff0000',
 'ffffff',
-'ee1166',
+'ffeeff',
 
 '00ffff',
 'eeeeee',
@@ -162,13 +168,13 @@ INSERT INTO themes VALUES (
 -- tables
 'ffffff',
 '',
-'003366',
+'60659c',
 'ffffff',
-'006633',
-'ffffff',
+'dddddd',
+'4c4c4c',
 'ffff00',
 '777777',
-'ffdddd',
+'60659c',
 ''
 
 );
