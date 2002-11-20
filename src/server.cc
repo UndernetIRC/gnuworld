4df2ed2b0f21007d48c2e470ab477045b0b38cba
Dan Karrels <dan@undernet.org>
2002-11-20 22:09:34 +0000

    Minor changes

--- src/server.cc
+++ src/server.cc
@@ -23,7 +23,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
  * USA.
  *
- * $Id: server.cc,v 1.151 2002/11/07 20:13:56 dan_karrels Exp $
+ * $Id: server.cc,v 1.152 2002/11/20 22:09:34 dan_karrels Exp $
  */
 
 #include	<sys/time.h>
@@ -72,7 +72,7 @@
 #include	"Connection.h"
 
 const char server_h_rcsId[] = __SERVER_H ;
-const char server_cc_rcsId[] = "$Id: server.cc,v 1.151 2002/11/07 20:13:56 dan_karrels Exp $" ;
+const char server_cc_rcsId[] = "$Id: server.cc,v 1.152 2002/11/20 22:09:34 dan_karrels Exp $" ;
 const char config_h_rcsId[] = __CONFIG_H ;
 const char misc_h_rcsId[] = __MISC_H ;
 const char events_h_rcsId[] = __EVENTS_H ;
@@ -385,6 +385,7 @@ elog	<< "xServer> Loaded "
 	<< endl ;
 
 commandMapFile.close() ;
+
 return returnVal ;
 }
 
