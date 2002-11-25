d68b548fccd890f47f706364f9ebae12a94e6259
Jeekay <jeekay@undernet.org>
2002-11-25 03:54:05 +0000

    Updated to reflect that nickserv and cservice can now coexist. Changed definition of nickserv table

--- doc/nickserv/nickserv.sql
+++ doc/nickserv/nickserv.sql
@@ -7,6 +7,7 @@ CREATE TABLE "users" (
 	"registered_ts" integer DEFAULT 0 NOT NULL,
 	"flags" integer DEFAULT 0 NOT NULL,
 	"level" smallint DEFAULT 0 NOT NULL,
+  "logmask" smallint DEFAULT 0 NOT NULL,
 	Constraint "users_pkey" Primary Key ("id")
 );
 
