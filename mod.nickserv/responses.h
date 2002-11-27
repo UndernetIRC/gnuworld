657395a7d7d888c293c3bb3248027164c5afd946
Jeekay <jeekay@undernet.org>
2002-11-27 18:46:50 +0000

    Standard command responses

---
+++ mod.nickserv/responses.h
@@ -0,0 +1,24 @@
+/*
+ * This file statically defines the various messages used globally in the module
+ */
+
+#ifndef RESPONSES_H
+#define RESPONSES_H "$Id: responses.h,v 1.1 2002/11/27 18:46:50 jeekay Exp $"
+
+namespace gnuworld {
+
+namespace ns {
+
+namespace responses {
+
+  const string noAccess("Sorry, you do not have access to this command.");
+  
+  const string noSuchUser("Sorry, that user name does not exist.");
+
+} // namespace responses
+
+} // namespace ns
+
+} // namespace gnuworld
+
+#endif
