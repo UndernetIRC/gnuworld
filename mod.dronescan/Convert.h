b49f90d4212373b46b8b263bb2e71963c34e043c
Jeekay <jeekay@undernet.org>
2003-10-12 16:22:53 +0000

    Move code from headers to source files

--- mod.dronescan/Convert.h
+++ mod.dronescan/Convert.h
@@ -1,11 +1,9 @@
 #ifndef CONVERT_H
-#define CONVERT_H "$Id: Convert.h,v 1.3 2003/08/05 14:56:15 jeekay Exp $"
+#define CONVERT_H "$Id: Convert.h,v 1.4 2003/10/12 16:22:53 jeekay Exp $"
 
 #include <deque>
-#include <sstream>
 #include <string>
 
-#include <stdio.h>
 #include <time.h>
 
 namespace gnuworld {
@@ -14,62 +12,13 @@ typedef std::deque<std::string> assembleType;
 
 class Convert {
 public:
-	inline static std::string ConvertSecondsToString(time_t _seconds)
-	{
-		assembleType toAssm;
-		char temp[20];
-
-		time_t seconds = _seconds % 60;
-		_seconds -= seconds;
-		_seconds /= 60;
-		
-		snprintf(temp, 20, "%lis", seconds);
-		toAssm.push_front(temp);
-		
-		if(_seconds <= 0) {
-			return Assemble(toAssm);
-		}
-		
-		time_t minutes = _seconds % 60;
-		_seconds -= minutes;
-		_seconds /= 60;
-		
-		snprintf(temp, 20, "%lim", minutes);
-		toAssm.push_front(temp);
-		
-		if(_seconds <= 0) {
-			return Assemble(toAssm);
-		}
-		
-		time_t hours = _seconds % 24;
-		_seconds -= hours;
-		_seconds /= 24;
-		
-		snprintf(temp, 20, "%lih", hours);
-		toAssm.push_front(temp);
-		
-		if(_seconds <= 0) {
-			return Assemble(toAssm);
-		}
-		
-		time_t days = _seconds;
-		
-		snprintf(temp, 20, "%lid", days);
-		toAssm.push_front(temp);
-		
-		return Assemble(toAssm);
-	}
+	static std::string ConvertSecondsToString(time_t _seconds);
 	
-	inline static std::string Assemble(const assembleType& toAssm)
-	{
-		std::stringstream Return;
-		for(assembleType::const_iterator itr = toAssm.begin() ;
-		    itr != toAssm.end() ; ++itr) {
-			Return << *itr;
-		}
-		
-		return Return.str();	
-	}
+	static std::string Assemble(const assembleType& toAssm);
+	
+protected:
+	/* Don't instantiate a static only class */
+	Convert() { } ;
 }; // class Convert
 
 } // namespace gnuworld
