9b5d6fa350afce28fc5d8efd702395bee02d0172
nighty <nighty@undernet.org>
2003-10-26 11:55:35 +0000

    Need to set the default value when updating an existing DB

--- doc/update_complaints_20031026.sql
+++ doc/update_complaints_20031026.sql
@@ -1,8 +1,9 @@
--- $Id: update_complaints_20031026.sql,v 1.1 2003/10/26 11:22:28 nighty Exp $
+-- $Id: update_complaints_20031026.sql,v 1.2 2003/10/26 11:55:35 nighty Exp $
 --
 -- Use this file if you have a database schema older than oct 26 2003.
 -- /usr/local/pgsql/bin/psql cservice < update_complaints_20031026.sql
 --
 
 ALTER TABLE "complaints" ADD "current_owner" int4 NOT NULL;
+UPDATE complaints SET current_owner=0;
 
