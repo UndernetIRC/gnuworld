5012c3ff42d9b1619f6fc711abb41ca761aaeecb
Dan Karrels <dan@undernet.org>
2004-08-24 23:49:29 +0000

    Just updated build system

--- include/gw_hashmap.h
+++ include/gw_hashmap.h
@@ -1,7 +1,30 @@
+/**
+ * gw_hashmap.h
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
+ * USA.
+ *
+ * $Id: gw_hashmap.h,v 1.2 2004/08/24 23:49:34 dan_karrels Exp $
+ */
+
 #ifndef GW_HASHMAP_H
 #define GW_HASHMAP_H
 
-#include "defs.h"
+#include	<string>
+
+#include	"defs.h"
 
 /* If we have ext/hash_map, it is probably the GNU GCC extension.
  * That lives in the extension namespace of __gnu_cxx.
