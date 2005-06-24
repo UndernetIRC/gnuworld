ee10095c28302ace1eb18c35083a1308cbdbb786
Dan-m00 <daniel@undernet.org>
2005-06-24 00:20:45 +0000

    updated 'list' command to include a new command:
    'list channels key X' where X is a channel key or wildcard key
    
    returns all matching channels (up to 15) and a count to the user.

--- mod.ccontrol/LISTCommand.cc
+++ mod.ccontrol/LISTCommand.cc
@@ -17,7 +17,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
  * USA.
  *
- * $Id: LISTCommand.cc,v 1.13 2005/01/12 03:50:29 dan_karrels Exp $
+ * $Id: LISTCommand.cc,v 1.14 2005/06/24 00:20:45 kewlio Exp $
  */
 
 #include	<string>
@@ -26,11 +26,13 @@
 #include	<cstdlib>
 
 #include	"ccontrol.h"
+#include	"Network.h"
+#include	"Channel.h"
 #include	"CControlCommands.h"
 #include	"StringTokenizer.h"
 #include	"gnuworld_config.h"
 
-RCSTAG( "$Id: LISTCommand.cc,v 1.13 2005/01/12 03:50:29 dan_karrels Exp $" ) ;
+RCSTAG( "$Id: LISTCommand.cc,v 1.14 2005/06/24 00:20:45 kewlio Exp $" ) ;
 
 namespace gnuworld
 {
@@ -42,6 +44,10 @@ namespace uworld
 
 bool LISTCommand::Exec( iClient* theClient, const string& Message)
 {	 
+typedef list<const Channel*> channelListT;
+const Channel* curChannel;
+channelListT channelList;
+unsigned int count = 0;
 
 StringTokenizer st( Message ) ;
 if(st.size() < 2)
@@ -71,6 +77,41 @@ else if(!strcasecmp(st[1].c_str(),"badchannels"))
 else if(!strcasecmp(st[1].c_str(),"exceptions"))
 	{
 	bot->listExceptions(theClient);
+	}
+else if(!strcasecmp(st[1].c_str(),"channels"))
+	{
+		/* new "list channels" command */
+		if (!strcasecmp(st[2].c_str(),"key"))
+		{
+			if (st.size() < 3)
+			{
+				bot->Notice(theClient, "for 'list channels key', you must supply the key to search for!");
+				return false;
+			}
+			/* ok, we have the key to search for in st[3] */
+			channelList = Network->getChannelsWithKey(st[3]);
+
+			if (channelList.empty())
+			{
+				bot->Notice(theClient, "I'm sorry, no channels have a key matching '%s'", st[3].c_str());
+				return false;
+			}
+
+			bot->Notice(theClient, "--==[ Channels matching key '%s' %s]==--", st[3].c_str(),
+				(channelList.size() > 15)?"- showing first 15 only":"");
+
+			for (channelListT::iterator cptr = channelList.begin(); (cptr != channelList.end()) && (count < 15); cptr++)
+			{
+				curChannel = *cptr;
+				bot->Notice(theClient, "Channel: %s", curChannel->getName().c_str());
+				count++;
+			}
+			bot->Notice(theClient, "--==[ End of 'list channels key %s' - %d found ]==--",
+				st[3].c_str(), channelList.size());
+		} else {
+			bot->Notice(theClient,"'list channels' requires 'key'");
+			return false;
+		}
 	}
 else
 	{
