848232162f5ff38a35ba256759c6413b88b2ac34
Dan-m00 <daniel@undernet.org>
2007-04-18 11:06:06 +0000

    added extra debugging info

--- src/server.cc
+++ src/server.cc
@@ -23,7 +23,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
  * USA.
  *
- * $Id: server.cc,v 1.221 2007/04/18 10:23:39 kewlio Exp $
+ * $Id: server.cc,v 1.222 2007/04/18 11:06:06 kewlio Exp $
  */
 
 #include	<sys/time.h>
@@ -70,7 +70,7 @@
 #include	"ConnectionHandler.h"
 #include	"Connection.h"
 
-RCSTAG( "$Id: server.cc,v 1.221 2007/04/18 10:23:39 kewlio Exp $" ) ;
+RCSTAG( "$Id: server.cc,v 1.222 2007/04/18 11:06:06 kewlio Exp $" ) ;
 
 namespace gnuworld
 {
@@ -1794,7 +1794,10 @@ if( !chanModes.empty() &&
 						<< " number of arguments to chanModes "
 						<< " in 'l' ("
 						<< chanModes
-						<< ")"
+						<< ") for "
+						<< theClient->getNickName()
+						<< " in "
+						<< chanName
 						<< endl ;
 					break ;
 					}
