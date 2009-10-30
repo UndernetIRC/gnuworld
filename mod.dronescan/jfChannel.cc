e6cea34750d9aab5624217d88b39460e0d464ad3
MrBean <MrBean@undernet.org>
2009-10-30 20:16:46 +0000

    Fixed a compilation error

--- mod.dronescan/jfChannel.cc
+++ mod.dronescan/jfChannel.cc
@@ -27,25 +27,32 @@ namespace ds {
 
 void jfChannel::addJoin(iClient* theClient)
 {
-std::string clientIp = xIP(theClient->getIP()).GetNumericIP();
-jfClientData& jfClient = joinPartMap[clientIp];
-jfClient.numOfJoins++;
-std::list<std::string>::const_iterator begin = jfClient.numerics.begin();
-std::list<std::string>::const_iterator end = jfClient.numerics.end();
+	std::string clientIp = xIP(theClient->getIP()).GetNumericIP();
+	jfClientData& jfClient = joinPartMap[clientIp];
+	jfClient.numOfJoins++;
+	std::list<std::string>::iterator begin = jfClient.numerics.begin();
+	std::list<std::string>::iterator end = jfClient.numerics.end();
 
-if(theClient->isOper())
-	{
-	jfClient.seenOper = true;
+	if(theClient->isOper())
+		{
+		jfClient.seenOper = true;
+		}
+	if(theClient->getMode(iClient::MODE_REGISTERED))
+		{
+		jfClient.seenLoggedInUser = true;
+		jfClient.userNames.push_back(std::pair<std::string,std::string>(theClient->getCharYYXXX(),
+			theClient->getAccount()));
+		}
+
+	bool found = false;
+	for(std::list<std::string>::iterator it=begin;it!=end && !found;++it) {
+		if(*it == theClient->getCharYYXXX()) {
+			found = true;
+		}
 	}
-if(theClient->getMode(iClient::MODE_REGISTERED))
-	{
-	jfClient.seenLoggedInUser = true;
-	jfClient.userNames.push_back(std::pair<std::string,std::string>(theClient->getCharYYXXX(),
-		theClient->getAccount()));
-	}
-if(find(begin,end,theClient->getCharYYXXX()) == end)
-	{
-	jfClient.numerics.push_back(theClient->getCharYYXXX());
+
+	if(!found) {
+		jfClient.numerics.push_back(theClient->getCharYYXXX());
 	}
 }
 	
